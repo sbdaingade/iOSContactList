4a2e0cca34f441ba221db50772c8f445f0143526
Sachin Daingade <sbdaingade@gmail.com>
2019-10-31 10:15:20 +0530

    first commmit

---
+++ iOSContactList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSContactList
+//
+//  Created by Sachin Daingade on 31/10/19.
+//  Copyright © 2019 Sachin Daingade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
